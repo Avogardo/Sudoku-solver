9bfd6f831ffae6530dfc37051001715277c7a65d
Avogardo <jakub.i.wolny@gmail.com>
2018-05-12 00:03:41 +0200

    created main and helper files

---
+++ helper.js
@@ -0,0 +1,48 @@
+const clone = existingArray => {
+  let newObj = (existingArray instanceof Array) ? [] : {};
+  for (i in existingArray) {
+    if (i == 'clone') continue;
+    if (existingArray[i] && typeof existingArray[i] == "object") {
+      newObj[i] = clone(existingArray[i]);
+    } else {
+      newObj[i] = existingArray[i]
+    }
+  }
+  return newObj;
+};
+
+const arrays_subtraction = (array1, array2) => {
+  const newArray = [];
+
+  array1.forEach(element => {
+    const areBoth = array2.find(element2 => element2 === element);
+
+    if (!areBoth) {
+      newArray.push(element);
+    }
+  });
+
+  return newArray;
+};
+
+const updateLoader = isWorking => {
+  document.querySelector('.loader-wrapper').style.zIndex = isWorking ? 10 : -1;
+  document.querySelector('.loader-wrapper').style.opacity = isWorking ? 1 : 0;
+};
+
+const showGrid = grid => {
+  console.log(grid);
+};
+
+const printGrid = (grid, elementId) => {
+  let table = '';
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      table += `${grid[row][col]} `;
+    }
+    table += '<br>';
+  }
+
+  document.getElementById(elementId).innerHTML = table;
+  console.log(grid);
+};
---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('buttonOne').addEventListener('click', () => {
+    const sudokuCounterInputValue = Number(document.getElementById('sudoku-counter-input').value);
+    const numberOfSolve =  sudokuCounterInputValue === 0 ? 1 : sudokuCounterInputValue;
+
+    let grid = clone(DATA[0]);
+
+    new JumpingStudent(grid, numberOfSolve);
+
+    grid = clone(DATA[0]);
+
+    const startTime = performance.now();
+    main(grid, numberOfSolve);
+    const endTime = performance.now();
+    console.log('It took ' + (endTime - startTime) + ' ms.');
+    // document.getElementById('result').textContent = 'It took ' + (endTime - startTime) + ' ms. (result in console (F12))';
+    printGrid(grid, 'outputGrid');
+  });
+});
