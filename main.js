feca7a402a65ac275cf4b73bf183fe841f1551ce
Avogardo <jakub.i.wolny@gmail.com>
2018-05-12 00:05:09 +0200

    show input grid on page

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+  printGrid(DATA[0], 'inputGrid');
+
   document.getElementById('buttonOne').addEventListener('click', () => {
     const sudokuCounterInputValue = Number(document.getElementById('sudoku-counter-input').value);
     const numberOfSolve =  sudokuCounterInputValue === 0 ? 1 : sudokuCounterInputValue;
